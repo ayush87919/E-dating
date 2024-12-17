ed9a2f2428eb145d2b0b22c084b0df2ec6b95afd
Ayush Tyagi <2023csb1108@iitrpr.ac.in>
2024-12-17 20:59:58 +0530

    dating app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Import Material UI ThemeProvider and createTheme
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline'; // Optional for consistent styles
+
+// Create a Material UI theme (you can customize it as needed)
+const theme = createTheme();
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}> {/* Provide the theme to your app */}
+    <CssBaseline /> {/* Reset baseline CSS */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measure performance (optional)
 reportWebVitals();
